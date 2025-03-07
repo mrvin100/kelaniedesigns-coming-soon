273dae7bf5dbfe1717349b0776673d422e37ae93
mrvin100 <vincentyoumssi@example.com>
2025-03-07 09:16:16 +0100

    KEL-11 : remove HeadingBar component

--- components/coming-soon/ui/index.tsx
+++ components/coming-soon/ui/index.tsx
@@ -2,6 +2,5 @@ import { SectionBackground } from "./section-background";
 import { Button } from "./button";
 import { Logo } from "./logo";
 import { Input } from "./input";
-import { HeadingBar } from "./heading-bar";
 
-export { SectionBackground, Button, Logo, Input, HeadingBar };
+export { SectionBackground, Button, Logo, Input };
